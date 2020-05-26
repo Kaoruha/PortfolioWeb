033b863f035a9a703179c2148b0511abde7624a5
Sunyu <33867159+Kaoruha@users.noreply.github.com>
2020-05-26 17:24:26 +0800

    Update Setting.vue

--- web/src/pages/Setting.vue
+++ web/src/pages/Setting.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
     <h1>Setting</h1>
+    <input type="file">
+    <q-btn @click="submit">提交</q-btn>
   </div>
 </template>
 
 <script>
-
+  import Tag from '../api/tag.js'
   export default {
     name: 'Setting',
     components: {},
     data() {
       return {}
     },
-    methods: {}
+    methods: {
+      submit() {
+        Tag.Upload()
+        alert()
+      }
+    }
   }
 </script>
 
